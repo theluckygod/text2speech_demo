ab15bba51fd6ca45871950c700ca14588926d19a
theluckygod <thieu216777@gmail.com>
2021-05-31 10:55:19 +0700

    api with queue

--- api.py
+++ api.py
@@ -5,6 +5,15 @@ from init_api import load_model, inference, load_conf
 from scipy.io.wavfile import write
 import numpy as np
 
+import time
+import threading
+from queue import Empty, Queue
+
+BATCH_SIZE = 1
+BATCH_TIMEOUT = 0.01
+CHECK_INTERVAL = 0.01
+requests_queue = Queue()
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -38,6 +47,46 @@ parser.add_argument('accent', help='Accent of speech')
 parser.add_argument('speed', help='Speech of speech')
 parser.add_argument('sr', type=int, help='Sampling rate of speech')
 
+def handle_requests():
+    """
+    This function handle requests.
+    """
+    while True:
+        requests_batch = []
+        while not (
+            len(requests_batch) > BATCH_SIZE or
+            (len(requests_batch) > 0 and time.time() - requests_batch[0]['time'] > BATCH_TIMEOUT)
+        ):
+            try:
+                requests_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
+            except Empty:
+                continue
+        texts = [request['input']['text'] for request in requests_batch]
+        accents = [request['input']['accent'] for request in requests_batch]
+        speeds = [request['input']['speed'] for request in requests_batch]
+        sampling_rates = [request['input']['sr'] for request in requests_batch]
+
+        try:
+            sentence = texts[0]
+            accent = accents[0]
+            speed = speeds[0]
+            sampling_rate = sampling_rates[0]
+            request = requests_batch[0]
+
+            data = inference(model_text2mel, 
+                            model_mel2audio, 
+                            denoiser, 
+                            sentence, 
+                            accent, 
+                            speed, 
+                            sampling_rate)
+            request['output'] = data
+        except:
+            request['output'] = "Fail"
+            continue
+
+threading.Thread(target=handle_requests).start()
+
 
 # Todo
 class Inference_e2e(Resource):
@@ -46,22 +95,20 @@ class Inference_e2e(Resource):
         sentence, accent, speed, sampling_rate = args['text'], args['accent'], args['speed'], args['sr']
         abort_if_config_doesnt_exist(accent, speed, sampling_rate)
 
-        try:
-            data = inference(model_text2mel, 
-                            model_mel2audio, 
-                            denoiser, 
-                            sentence, 
-                            accent, 
-                            speed, 
-                            sampling_rate)
-        except:
-            print("Fail to infer")
-            return None, 403
+        crequest = {'input': args, 'time': time.time()}
 
-        if isinstance(data, np.ndarray):
-            data = data.tolist()
-        return {"data": data, "sr": sampling_rate}, 201
+        requests_queue.put(crequest)
 
+        while 'output' not in crequest:
+            time.sleep(CHECK_INTERVAL)
+
+        data = crequest['output']
+
+        if not isinstance(data, np.ndarray):
+            return {"data": None, "sr": sampling_rate}, 403 # Fail to infer
+            
+        data = data.tolist()
+        return {"data": data, "sr": sampling_rate}, 201 # infer successfully
 
 ##
 ## Actually setup the Api resource routing here
