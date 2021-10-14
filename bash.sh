af596ee3d2ce85bcafc0258bb05c321c84fb4a48
sasukepn1999 <36978783+sasukepn1999@users.noreply.github.com>
2021-10-14 23:19:15 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # download pretrained models
-wget -O ./FastSpeech2/cptk/100000.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Eddrt4TbzZBNmmsDmUTsx1cBKl83DN7gC1sRlo45TLvKtg?e=jjaFwi\&download=1
+wget -O ./FastSpeech2/ckpt/100000.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Eddrt4TbzZBNmmsDmUTsx1cBKl83DN7gC1sRlo45TLvKtg?e=jjaFwi\&download=1
 wget -O ./FastSpeech2/hifigan/generator_universal.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Ec_tC7hVcFVJlTgOfXYDkjEBuvm_f4BhqEwtQc70k0hCHQ?e=Sn5p4g\&download=1
 # unzip tacotron2_checkpoints.zip
 # unzip hifi-gan_checkpoints.zip
