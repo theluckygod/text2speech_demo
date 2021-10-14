cc2d347ed2aafaafe557ab35d03c10d6e82e0563
sasukepn1999 <36978783+sasukepn1999@users.noreply.github.com>
2021-10-14 17:38:03 +0700

    Rollback
    Cap nhat lai ban cu vi commit nham len master

--- app.py
+++ app.py
@@ -4,50 +4,37 @@ from requests import post
 import numpy as np
 import yaml
 
-
 @st.cache
 def load_conf():
     with open("config.yml", "r", encoding="utf-8") as ymlfile:
         cfg = yaml.load(ymlfile, yaml.Loader)
     return cfg
 cfg = load_conf()
-    
-# # Side bar
-# st.sidebar.subheader('Accent')
-# accent_values = cfg["conf_values"]["accent"]
-# accent_default = cfg["conf_default"]["accent"]
-# accent = st.sidebar.selectbox('', accent_values, index=accent_values.index(accent_default))
 
 # Side bar
-st.sidebar.subheader('Speaker')
-speaker_values = list(range(cfg['conf_nof_speaker']['fastspeech2']))
-speaker_id = st.sidebar.selectbox('', speaker_values, index=0)
+st.sidebar.subheader('Accent')
+accent_values = cfg["conf_values"]["accent"]
+accent_default = cfg["conf_default"]["accent"]
+accent = st.sidebar.selectbox('', accent_values, index=accent_values.index(accent_default))
         
 st.sidebar.subheader('Speed')
 speed_values = cfg["conf_values"]["speed"]
 speed_default = cfg["conf_default"]["speed"]
 speed = st.sidebar.selectbox('', speed_values, index=speed_values.index(speed_default))
-
 st.sidebar.subheader('Sampling rate')
 sampling_rate_values = cfg["conf_values"]["sampling_rate"]
 sampling_rate_default = cfg["conf_default"]["sampling_rate"]
 sampling_rate = st.sidebar.selectbox('', sampling_rate_values, index=sampling_rate_values.index(sampling_rate_default))
-
-
 default_value_text_area = cfg['conf_st_app']['default_value_text_area']
 max_height_text_area = None #cfg['conf_st_app']['max_height_text_area']
 default_audio_path = cfg['conf_st_app']['default_audio_path']
 output_audio_path = cfg['conf_st_app']['output_audio_path']
-
-# Content
-st.title("Text to speech")
-
-sentence = st.text_area(label='Input your text here:',
+	@@ -37,20 +40,27 @@
                         value=default_value_text_area,
-                        height=max_height_text_area)
-        
+                        height=max_height_text_area) 
+
 if st.button("Generate"):
-    data = {'text': sentence, 'speaker_id': speaker_id, 'speed': speed, 'sr': sampling_rate}
+    data = {'text': sentence, 'accent': accent, 'speed': speed, 'sr': sampling_rate}
     print("\n------------------------------------")
     print(f"POST {data}")
 
@@ -69,4 +56,3 @@ if st.button("Generate"):
             print("Cannot write audio file!!!")
     else:
         st.text('No response from server.')
-        
