36c618cca23de2a5aba3101131a09a93db322e52
sasukepn1999 <36978783+sasukepn1999@users.noreply.github.com>
2021-10-21 15:10:36 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,7 +7,5 @@ pip install -r requirements.txt
 mkdir ./FastSpeech2/ckpt
 
 # download pretrained models
-wget -O ./FastSpeech2/ckpt/300000.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Ea_RN9BYVKJNgW2fpr1tXx8B514SWxDwm2SUdmrf85HgxA?e=vDdZ2W\&download=1
-wget -O ./FastSpeech2/hifigan/generator_universal.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Ec_tC7hVcFVJlTgOfXYDkjEBuvm_f4BhqEwtQc70k0hCHQ?e=Sn5p4g\&download=1
-# unzip tacotron2_checkpoints.zip
-# unzip hifi-gan_checkpoints.zip
+wget -O ./FastSpeech2/ckpt/300000.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Ea_RN9BYVKJNgW2fpr1tXx8B514SWxDwm2SUdmrf85HgxA?e=vDdZ2W\&download=1# model pretrained fastspeech
+wget -O ./FastSpeech2/hifigan/generator_universal.pth.tar https://studenthcmusedu-my.sharepoint.com/:u:/g/personal/1712791_student_hcmus_edu_vn/Ec6TjvI8oIZNrSfqlKM8xJIB13E_z8QNSVLuz7kySBeoYQ?e=xlPb99\&download=1# model pretrained hifi-gan
